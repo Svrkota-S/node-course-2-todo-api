481307b36c0e41e698a92b8f3a83e78eed64caaa
Stefan Svrkota <stefan@cramick-it.com>
2017-10-24 20:59:59 +0200

    Added POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -13,6 +13,8 @@ var app = express();
 
 app.use(bodyParser.json());
 
+// TODOS
+
 app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
@@ -100,6 +102,20 @@ app.patch('/todos/:id', (req, res) => {
   }).catch((err) => {
     res.status(400).send();
   })
+});
+
+// USERS
+
+app.post('/users', (req, res) => {
+  var user = new User(_.pick(req.body, ['email', 'password']));
+
+  user.save().then(() => {
+      return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((err) => {
+    res.status(400).send(err);
+  });
 });
 
 app.listen(process.env.PORT, () => {
