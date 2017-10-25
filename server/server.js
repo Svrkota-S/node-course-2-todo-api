919fe95d3e00d3b1820bd8896257140560db1562
Stefan Svrkota <stefan@cramick-it.com>
2017-10-25 14:12:48 +0200

    Added POST /users/login route

--- server/server.js
+++ server/server.js
@@ -108,7 +108,8 @@ app.patch('/todos/:id', (req, res) => {
 // USERS
 
 app.post('/users', (req, res) => {
-  var user = new User(_.pick(req.body, ['email', 'password']));
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
 
   user.save().then(() => {
       return user.generateAuthToken();
@@ -122,6 +123,19 @@ app.post('/users', (req, res) => {
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
+
+// POST /users/login { email, password }
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    })
+  }).catch((err) => {
+    res.status(400).send();
+  });
+});
+
 
 app.listen(process.env.PORT, () => {
   console.log(`Started on port ${process.env.PORT}`);
