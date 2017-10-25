88f5caa5b675945303d8dbe70ae9138bf103bebe
Stefan Svrkota <stefan@cramick-it.com>
2017-10-25 14:46:17 +0200

    Added tests for POST /users/login route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -215,7 +215,9 @@ describe('POST /users', () => {
           expect(user.email).toBe(email);
           expect(user.password).not.toBe(password);
           done();
-        })
+        }).catch((err) => {
+          done(err);
+        });
       });
   });
 
@@ -240,6 +242,57 @@ describe('POST /users', () => {
       .expect(400)
       .end(done);
   });
+});
 
+describe('POST /users', () => {
+  it('should login user and return auth token', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toBeTruthy();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
 
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens[0].access).toBe('auth');
+          expect(user.tokens[0].token).toBe(res.headers['x-auth']);
+          done();
+        }).catch((err) => {
+          done(err);
+        });
+      });
+  });
+
+  it('should reject invalid login', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password + 'something'
+      })
+      .expect(400)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toBeFalsy();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((err) => {
+          done(err);
+        });
+      });
+  });
 });
